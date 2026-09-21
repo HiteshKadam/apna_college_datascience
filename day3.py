532e3a8e129ae64bf3b3a24f533368be075ad4b3
HiteshKadam <hdkadam2000@gmail.com>
2026-09-21 23:49:36 +0530

    day 3.1

---
+++ day3.py
@@ -0,0 +1,26 @@
+#Strings
+
+word1 = "I love"
+word2 = "python"
+
+#concat
+print(word1 +" "+ word2)
+
+# Indexing
+print(word2[2])
+
+#Slicing
+sentence = "I only study from apna college"
+print(sentence[13:])
+
+#String formating
+a =5
+b = 10
+print(f"sum of {a} and {b} is 15")
+
+
+
+
+
+
+
