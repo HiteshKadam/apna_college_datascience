1f9a0cfed21a7cecbc20b4e6f993d9d433bbf974
HiteshKadam <hdkadam2000@gmail.com>
2026-09-22 23:57:41 +0530

    day 3.2

--- day3.py
+++ day3.py
@@ -19,8 +19,15 @@ b = 10
 print(f"sum of {a} and {b} is 15")
 
 
+#List
+numbers = [12,2,3,345,363,463,412,13]
 
-
-
+print(type(numbers))
+print(numbers[0:len(numbers)])
+numbers.append(0)
+numbers.insert(1,10)
+numbers.sort()
+numbers.reverse()
+print(numbers)
 
 
